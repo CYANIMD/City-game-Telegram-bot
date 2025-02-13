18035c99a57acd812171f169d9919db3976da55e
Игнатьев Михаил <CYANIMD@gmail.com>
2025-02-13 13:53:39 +0300

    Изменил структуру БД, реализовал методы check_first_symbol(), clear_used_cities(), add_city() и add_game(). Обнаружил ошибку с поиском городов, содержащих несколько заглавных букв

--- City-game-Telegram-bot/func.py
+++ City-game-Telegram-bot/func.py
@@ -2,33 +2,66 @@
 import sqlite3 as sql
 
 database_title = "info.db"
-ban_symbol = ['ы', 'ъ', 'ь'] #"Запретные" буквы
+ban_symbols = ['ы', 'ъ', 'ь'] # "Запретные" буквы
 
-#Возвращает последний символ слова в соответствии с правилами игры в города
+# Возвращает последний символ слова в соответствии с правилами игры в города
 def get_last_char(word: str) -> str:
-    return word[-1].upper() if word[-1] not in ban_symbol else word[-2].upper()
-#Возвращает случайный город из БД
+    if (len(word) < 2):
+        raise ValueError("len(word) < 2")
+    return word[-1].upper() if word[-1] not in ban_symbols else word[-2].upper()
+# Возвращает случайный город
 def get_random_city(previous_city: str) -> str:
     database = sql.connect(database_title)
     cursor = database.cursor()
     last_char = get_last_char(previous_city)
-    cursor.execute(f"SELECT name FROM cities WHERE name LIKE \"{last_char}%\"")
+    cursor.execute(f"SELECT name FROM world_cities WHERE name LIKE \"{last_char}%\"")
     results = cursor.fetchall()
     database.close()
     return None if results is None else random.choice(results)[0]
-#Проверяет, был ли назван указанный город ранее
+# Проверяет, был ли назван ранее указанный город
 def is_reused_city(city_name: str, chat_id: int) -> bool:
     database = sql.connect(database_title)
     cursor = database.cursor()
-    cursor.execute(f"SELECT * FROM chats WHERE city_name = \"{city_name}\" AND chat_id = \"{chat_id}\"")
+    cursor.execute(f"SELECT * FROM used_cities WHERE city_name = \"{city_name}\" AND chat_id = \"{chat_id}\"")
     result = cursor.fetchone()
     database.close()
     return result is not None
-#Проверяет наличие города в БД
+# Проверяет наличие города в БД
 def has_city(city_name: str) -> bool:
     database = sql.connect(database_title)
     cursor = database.cursor()
-    cursor.execute(f"SELECT city_id, name FROM cities WHERE name = \"{city_name.capitalize()}\"" )
+    cursor.execute(f"SELECT * FROM world_cities WHERE name = \"{city_name.capitalize()}\"" )
     result = cursor.fetchone()
     database.close()
-    return result is None
+    return result is not None
+# Добавляет или обновляет игру с указанием статуса
+def add_game(chat_id: int, is_game_start: bool):
+    database = sql.connect(database_title)
+    cursor = database.cursor()
+    cursor.execute(f"INSERT INTO games (chat_id, is_game_start) VALUES ({chat_id}, {is_game_start})" + 
+                   f"ON CONFLICT (chat_id) DO UPDATE SET is_game_start = {is_game_start}")
+    database.commit()
+    database.close()
+# Добавляет названный город в БД
+def add_city(chat_id: int, city_name: str):
+    database = sql.connect(database_title)
+    cursor = database.cursor()
+    cursor.execute(f"INSERT INTO used_cities (chat_id, city_name) VALUES ({chat_id}, \"{city_name}\")")
+    database.commit()
+    database.close()
+# Очещает использованные в указанном чате города
+def clear_used_cities(chat_id: int):
+    database = sql.connect(database_title)
+    cursor = database.cursor()
+    cursor.execute(f"DELETE FROM used_cities WHERE chat_id = {chat_id}")
+    database.commit()
+    database.close()
+# Проверяет правильность первого символа в названии города
+def check_first_symbol(city_name: str, chat_id: int) -> bool:
+    database = sql.connect(database_title)
+    cursor = database.cursor()
+    cursor.execute(f"SELECT last_symbol FROM games WHERE chat_id = {chat_id}")
+    temp = cursor.fetchone()
+    database.close()
+    return temp is not None and temp[0] is not None and city_name[0].upper() != temp[0][0].upper()
+        
--- City-game-Telegram-bot/main.py
+++ City-game-Telegram-bot/main.py
@@ -4,58 +4,57 @@ from telebot import types
 import sqlite3 as sql
 import random
 
-token = "" #Вставить свой токен
-bot = telebot.TeleBot(token) #Инициализация бота
+token = "" # Собственный токен
+bot = telebot.TeleBot(token) # Инициализация бота
 
-#Запуск бота
+# Приветствие (запуск бота)
 @bot.message_handler(commands=["start", "main"])
-def main(message):
+def welcome(message):
+    add_game(message.chat.id, False)
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("Да!", callback_data="Yes"),types.InlineKeyboardButton("Нет...", callback_data="No"))
-    bot.send_message(message.chat.id, "Привет! Я телеграм бот, с которым можно поиграть в города. Вот правила игры:"+
+    bot.send_message(message.chat.id, "Привет! Я телеграм бот, с которым можно поиграть \"города\". Опишу правила этой игры:"+
                      "\n(1). Один из игроков называет случайный город;"+
                      "\n(2). Каждый игрок по очереди называет город, который начинается на последнюю букву города (за некоторым исключением), названного предыдущим игроком;"+
                      "\n(3). Нельзя повторять города, которые уже были названы в ходе игры;"+
                      "\n(4). Города должны быть настоящими;"+
-                     "\n(5). Если город, названный игроком, заканчивается на букву \"ы\", \"ь\", \"ъ\" или \"й\", то следующий игрок должен назвать город, начинающийся с предпоследней буквы города, названного предыдущим игроком\n" +
+                     "\n(5). Если город, названный игроком, заканчивается на букву \"ы\", \"ь\", или \"ъ\", то следующий игрок должен назвать город, начинающийся с предпоследней буквы прошлого города\n" +
                      "Ты готов к игре?", reply_markup=markup)
-
-#Отслеживание нажатия на кнопку
+# Отслеживание нажатия на кнопку
 @bot.callback_query_handler(func=lambda call: True)
-def press_button(callback):
+def event_button(callback):
     match(callback.data):
         case "Yes":
             bot.send_message(callback.message.chat.id, "Приступаем к игре! Ты ходишь первым")
+            add_game(callback.message.chat.id, True)
         case "No":
             bot.send_message(callback.message.chat.id, "Поиграем в следующий раз, когда ты будешь готов")
         case _:
             bot.send_message(callback.message.chat.id, "Чта?!")
-#Пользователь вводит город
+# Ввод городов
 @bot.message_handler()
-def user_print_city(message):
-    if has_city(message.text):
+def print_cities(message):
+    user_city = message.text; # Город пользователя
+    if not(has_city(user_city)):
         bot.send_message(message.chat.id, "Такого города не существует. Попробуй ещё раз");
     elif is_reused_city(message.text, message.from_user.id):
-        bot.send_message(message.chat.id, "Этот город уже был! Назови другой город");
+        bot.send_message(message.chat.id, "Этот город был назван ранее. Назови другой город");
     else:
-        database = sql.connect(database_title)
-        cursor = database.cursor()
-        cursor.execute(f"SELECT symbol FROM last_symbols WHERE user_id = {message.from_user.id}")
-        temp = cursor.fetchone()
-        if temp is not None and message.text[0].upper() != temp[0].upper():
-            bot.send_message(message.chat.id, f"Этот город не на ту букву. Назови другой город на букву {temp[0].upper()}");
+        if check_first_symbol(message.text, message.chat.id):
+            bot.send_message(message.chat.id, f"Этот город не на ту букву. Назови другой город");
         else:
-            cursor.execute("INSERT INTO chats (chat_id, city_name) VALUES (?, ?)", (message.chat.id, message.text))
-            database.commit()
+            add_city(message.chat.id, message.text)
             current_city = get_random_city(message.text);
-            cursor.execute("INSERT INTO last_symbols (user_id, symbol)  VALUES (?, ?)  ON CONFLICT(user_id) DO UPDATE SET symbol = excluded.symbol", (message.from_user.id, get_last_char(current_city)))
+            database = sql.connect(database_title)
+            cursor = database.cursor()
+            cursor.execute(f"INSERT INTO games (chat_id, last_symbol)  VALUES ({message.chat.id}, \"{get_last_char(current_city)}\") ON CONFLICT(chat_id) DO UPDATE SET last_symbol = excluded.last_symbol")
             database.commit()
             if current_city is None:
                 bot.send_message(message.chat.id, f"Ты победил, я не могу назвать ни одного города")
             else:
-                cursor.execute("INSERT INTO chats (chat_id, city_name) VALUES (?, ?)", (message.chat.id, current_city))
+                cursor.execute(f"INSERT INTO used_cities (chat_id, city_name) VALUES ({message.chat.id}, \"{current_city}\")")
                 bot.send_message(message.chat.id, f"Хорошо, теперь мой город: {current_city}")
                 database.commit()
             database.close()
         
-bot.polling(none_stop=True)
+bot.polling(none_stop=True) # Бот постоянно работает
